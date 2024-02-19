ec1f23980e514547a423ac25ffcb3804270e4247
Donia <doniagardabou@gmail.com>
2024-02-19 14:41:19 +0100

    tp1 - ex1 - q4

--- tp1/ex1/main.cpp
+++ tp1/ex1/main.cpp
@@ -1,13 +1,17 @@
 #include <iomanip>
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Entre ton nom: ";
+    std::cout << "Entre ton nom: "; // écrit sur la sortie std
 
-    char name[20] = "";
-    std::cin >> std::setw(20) >> name;
-    std::cout << "Bonjour " << name << " !" << std::endl;
+    // char name[20] = ""; // aloue un tableau de char nommé name de taille 20
+    // std::cin >> std::setw(20) >> name;
+
+    std::string name;
+    std::cin >> name;
+    std::cout << "Bonjour " << name << " !" << std::endl;     // affiche un msg sur stdout
 
     return 0;
 }
